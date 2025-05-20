9d6bef9072e237fdf31816003db6bfe811e1694a
olaguna7 <oscarlagunacaminero@gmail.com>
2025-05-20 18:20:12 +0200

    Añadido metodo en el repositorio de reseñas para objtener la lista de objetos

--- src/main/java/com/atm/buenas_practicas_java/repositories/ResenaRepository.java
+++ src/main/java/com/atm/buenas_practicas_java/repositories/ResenaRepository.java
@@ -1,7 +1,11 @@
 package com.atm.buenas_practicas_java.repositories;
 
+import com.atm.buenas_practicas_java.entities.Objeto;
 import com.atm.buenas_practicas_java.entities.Resena;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ResenaRepository extends JpaRepository<Resena, Long> {
+    List<Resena> findResenasByObjeto(Objeto objeto);
 }
