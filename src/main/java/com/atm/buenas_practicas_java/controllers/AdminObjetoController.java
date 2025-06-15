0dceb29b748c70b6780a230f0cb2dba9f0eb4d7f
olaguna7 <oscarlagunacaminero@gmail.com>
2025-06-15 18:16:15 +0200

    Añadido formulario añadir nuevo objeto

--- src/main/java/com/atm/buenas_practicas_java/controllers/AdminObjetoController.java
+++ src/main/java/com/atm/buenas_practicas_java/controllers/AdminObjetoController.java
@@ -1,10 +1,13 @@
 package com.atm.buenas_practicas_java.controllers;
 
+import com.atm.buenas_practicas_java.dtos.ObjetoCrearDTO;
 import com.atm.buenas_practicas_java.services.ObjetoAPIService;
 import info.movito.themoviedbapi.tools.TmdbException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -25,5 +28,17 @@ public class AdminObjetoController {
     public String mostrarBusqueda(Model model, @RequestParam String query) throws TmdbException {
         model.addAttribute("resultado", objetoAPIService.buscarPeliculas(query));
         return "/admin-panel/nuevoobjeto";
+    }
+
+    @GetMapping("/admin/nuevoobjeto/form")
+    public String mostrarFormulario(@RequestParam Integer tmdbId, Model model) throws TmdbException {
+        ObjetoCrearDTO pelicula = objetoAPIService.obtenerDetallesCompletos(tmdbId);
+        model.addAttribute("pelicula", pelicula);
+        return "/admin-panel/form";
+    }
+
+    @PostMapping("/admin/nuevoobjeto/guardar")
+    public String guardarObjeto(@ModelAttribute ObjetoCrearDTO objetoDTO) {
+        return "redirect:/admin/nuevoobjeto";
     }
 }
--- src/main/java/com/atm/buenas_practicas_java/dtos/ObjetoCrearDTO.java
+++ src/main/java/com/atm/buenas_practicas_java/dtos/ObjetoCrearDTO.java
@@ -1,5 +1,6 @@
 package com.atm.buenas_practicas_java.dtos;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -7,6 +8,11 @@ public record ObjetoCrearDTO(
         String titulo,
         String imagenUrl,
         String trailerUrl,
-        LocalDateTime fechaPublicacion
+        String fechaPublicacion,
+        String sinopsis,
+        List<GeneroDTO> generos,
+        List<PersonaDTO> direccion,
+        List<PersonaDTO> reparto,
+        Integer tmdbId
 ) {
 }
