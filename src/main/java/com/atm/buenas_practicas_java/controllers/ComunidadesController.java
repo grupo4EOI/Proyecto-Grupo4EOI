a10438f2706d104b29f3b88ba4184fa3f65ddffe
juanjod197 <juanjod197@gmail.com>
2025-06-16 18:34:55 +0200

    quiero llorar

--- src/main/java/com/atm/buenas_practicas_java/controllers/ComunidadesController.java
+++ src/main/java/com/atm/buenas_practicas_java/controllers/ComunidadesController.java
@@ -8,7 +8,11 @@ import com.atm.buenas_practicas_java.entities.Usuario;
 import com.atm.buenas_practicas_java.mapper.ComunidadMapper;
 import com.atm.buenas_practicas_java.services.*;
 import com.atm.buenas_practicas_java.services.facade.ComunidadServiceFacade;
+import com.atm.buenas_practicas_java.utils.PaginacionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -37,18 +41,18 @@ public class ComunidadesController {
     }
 
     @GetMapping("/{id}/temas")
-    public String mostrarTemas(Model model, @PathVariable Long id) {
+    public String mostrarTemas(Model model, @PathVariable Long id, @PageableDefault(size = 20) Pageable pageable) {
         ComunidadSimpleDTO comunidad = comunidadServiceFacade.findById(id);
-        List<PublicacionDTO> publicaciones = comunidadServiceFacade.buscarPublicacionesPorComunidad(id);
+        Page<PublicacionDTO> publicaciones = PaginacionUtils.listToPage(comunidadServiceFacade.buscarPublicacionesPorComunidad(id), pageable);
         model.addAttribute("comunidad", comunidad);
         model.addAttribute("publicaciones", publicaciones);
         return "comunidad";
     }
 
     @GetMapping("/{idcom}/temas/{id}")
-    public String mostrarComentarios(Model model, @PathVariable Long idcom, @PathVariable Long id, @RequestParam(value = "citar", required = false) Long comentarioCitadoId) {
+    public String mostrarComentarios(Model model, @PathVariable Long idcom, @PathVariable Long id, @PageableDefault(size = 5) Pageable pageable) {
         ComunidadSimpleDTO comunidad = comunidadServiceFacade.findById(idcom);
-        List<ComentarioPublicacionSimpleDTO> comentarios = comunidadServiceFacade.buscarComentariosPorPublicacion(id);
+        Page<ComentarioPublicacionSimpleDTO> comentarios = PaginacionUtils.listToPage(comunidadServiceFacade.buscarComentariosPorPublicacion(id), pageable);
         model.addAttribute("comunidad", comunidad);
         model.addAttribute("comentarios", comentarios);
 
--- src/main/java/com/atm/buenas_practicas_java/repositories/PublicacionRepository.java
+++ src/main/java/com/atm/buenas_practicas_java/repositories/PublicacionRepository.java
@@ -2,6 +2,8 @@ package com.atm.buenas_practicas_java.repositories;
 
 import com.atm.buenas_practicas_java.entities.Comunidad;
 import com.atm.buenas_practicas_java.entities.Publicacion;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
