1541169694ac995958593320aaf81ce178dcff9c
olaguna7 <oscarlagunacaminero@gmail.com>
2025-06-17 17:08:43 +0200

    Revisadas urls constantes controladores

--- src/main/java/com/atm/buenas_practicas_java/controllers/ContactoController.java
+++ src/main/java/com/atm/buenas_practicas_java/controllers/ContactoController.java
@@ -1,8 +1,6 @@
 package com.atm.buenas_practicas_java.controllers;
 
 import com.atm.buenas_practicas_java.services.EmailService;
-import com.atm.buenas_practicas_java.services.ObjetoService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
